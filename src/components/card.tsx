a217aef4a3112e87a5f912a0071405c2539b84d1
ahadanfauzan123 <ahadanfauzan123@gmail.com>
2024-03-28 19:19:10 +0700

    develop3

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,22 @@
 "use client"
 import React from 'react'
+import { FaRegCircleCheck } from "react-icons/fa6";
 interface Props {
   name: string;
   price: number;
   custom: boolean;
+  pages: number;
+  dynamic: boolean;
+  uiux: boolean;
+  revision: number;
+  domain: boolean;
+  ssl: boolean;
+  bandwith: number;
+  storage: number;
+  socialMedia: boolean;
 }
 
-function Card({name, price, custom}:Props) {
+function Card({name, price, custom, pages, dynamic, uiux, revision, domain, ssl, bandwith, storage, socialMedia}:Props) {
   const formattedAmount = new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR'
@@ -26,46 +36,36 @@ function Card({name, price, custom}:Props) {
 </div>
 <ul role="list" className="space-y-5 my-7">
 <li className="flex items-center">
-<svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">2 team members</span>
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{pages<=8?(<>{pages} Pages</>):(<>Custom Pages</>)}</span>
 </li>
 <li className="flex">
-<svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">20GB Cloud storage</span>
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{bandwith<=20?(<>{bandwith} Bandwith</>):(<>Up To Unlimited Bandwith</>)}</span>
 </li>
 <li className="flex">
-<svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Integration help</span>
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{uiux===true?(<>Free UI/UX</>):(<>Custom UI/UX</>)}</span>
 </li>
-<li className="flex line-through decoration-gray-500">
-<svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 ms-3">Sketch Files</span>
+<li className="flex decoration-gray-500">
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 ms-3">{dynamic===true?(<>Dynamic Website</>):(<></>)}</span>
 </li>
-<li className="flex line-through decoration-gray-500">
-<svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 ms-3">API Access</span>
+<li className="flex decoration-gray-500">
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 ms-3">{revision<3?(<>{revision} Revision</>):<>Custom Number of Revision</>}</span>
 </li>
-<li className="flex line-through decoration-gray-500">
-<svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 ms-3">Complete documentation</span>
+<li className="flex decoration-gray-500">
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 ms-3">{domain===true?(<>Free Domain</>):(<>Free Domain</>)}</span>
 </li>
-<li className="flex line-through decoration-gray-500">
-<svg className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-</svg>
-<span className="text-base font-normal leading-tight text-gray-500 ms-3">24×7 phone & email support</span>
+<li className="flex decoration-gray-500">
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 ms-3">{ssl===true?(<>Free SSL</>):(<>Free SSL</>)}</span>
+</li>
+<li className="flex decoration-gray-500">
+<FaRegCircleCheck className="h-5 w-5 text-blue-500" />
+<span className="text-base font-normal leading-tight text-gray-500 ms-3">{storage<=5?(<>{storage} GB Storage</>):(<>Custom Storage</>)}</span>
 </li>
 </ul>
 <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
--- src/components/service.tsx
+++ src/components/service.tsx
@@ -18,9 +18,9 @@ function Service() {
     <div id='service' className='w-[90%] mx-auto min-h-screen flex flex-col space-y-8 justify-center items-center'>
       <h1 className="text-start text-[42px] font-extrabold "><span className="">Our Services</span></h1>
       <div className='hidden flex-[0.65] w-full lg:flex items-center justify-center space-x-5'>
-            <Card name="STARTER" price={3500000} custom={false} />
-            <Card name="BUSINESS" price={4000000} custom={false} />
-            <Card name="CUSTOM" price={3500000} custom={true} />
+            <Card name="STARTER" price={3500000} custom={false} pages={5} dynamic={true} uiux={true} revision={1} domain={true} ssl={true} bandwith={10} storage={5} socialMedia={false} />
+            <Card name="BUSINESS" price={4000000} custom={false} pages={8} dynamic={true} uiux={true} revision={1} domain={true} ssl={true} bandwith={20} storage={5} socialMedia={true} />
+            <Card name="CUSTOM" price={3500000} custom={true} pages={9} dynamic={true} uiux={false} revision={5} domain={true} ssl={true} bandwith={30} storage={6} socialMedia={true} />
       </div>   
       {/* sm */}
       <div className=' w-screen flex lg:hidden'>
@@ -35,13 +35,13 @@ function Service() {
           className="mySwiper"
         >
           <SwiperSlide>
-            <Card name="STARTER" price={3500000} custom={false} />
+            <Card name="STARTER" price={3500000} custom={false} pages={5} dynamic={true} uiux={true} revision={1} domain={true} ssl={true} bandwith={10} storage={5} socialMedia={false} />
           </SwiperSlide>
           <SwiperSlide>
-            <Card name="BUSINESS" price={4000000} custom={false} />
+            <Card name="BUSINESS" price={4000000} custom={false} pages={8} dynamic={true} uiux={true} revision={1} domain={true} ssl={true} bandwith={20} storage={5} socialMedia={true} />
           </SwiperSlide>
           <SwiperSlide>
-            <Card name="CUSTOM" price={3500000} custom={true} />
+            <Card name="CUSTOM" price={3500000} custom={true} pages={9} dynamic={true} uiux={false} revision={5} domain={true} ssl={true} bandwith={30} storage={6} socialMedia={true} />
           </SwiperSlide>
         </Swiper>
       </div>
